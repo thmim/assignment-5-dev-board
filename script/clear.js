8b59b5e88a67027998ce04d5debaef526a93c999
thmim <mdtaharimmim1010@gmail.com>
2025-03-02 02:18:05 +0600

    added clear history button

---
+++ script/clear.js
@@ -0,0 +1,4 @@
+document.getElementById('clear-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    document.getElementById("activity-data").innerHTML = "";
+})
--- script/click.js
+++ script/click.js
@@ -1,4 +1,5 @@
 document.getElementById('completed-btn1').addEventListener('click',function(event){
+    
     event.preventDefault();
     alert('Board Updated Successfully');
     const count = getInputValueById('assigned-count');
@@ -10,7 +11,7 @@ document.getElementById('completed-btn1').addEventListener('click',function(even
     const result = originalCount+1;
     setInnerTextByIDandValue('count-box',result);
 
-    const activityHistory = document.getElementById('activity-section');
+    const activityHistory = document.getElementById('activity-data');
     const div = document.createElement('div');
     const currentTime = new Date().toLocaleTimeString();
     const addTitle = document.getElementById('fix-title');
@@ -22,6 +23,36 @@ document.getElementById('completed-btn1').addEventListener('click',function(even
       `
     activityHistory.appendChild(div);
     const btn1 = document.getElementById('completed-btn1');
+    btn1.disabled=true;
+    btn1.style.backgroundColor = "gray";
+})
+
+
+document.getElementById('completed-btn2').addEventListener('click',function(event){
+    
+    event.preventDefault();
+    alert('Board Updated Successfully');
+    const count = getInputValueById('assigned-count');
+    // console.log(count);
+      const finalCount = count-1;
+    setInnerTextByIDandValue('assigned-count',finalCount);
+
+    const originalCount = getInputValueById('count-box');
+    const result = originalCount+1;
+    setInnerTextByIDandValue('count-box',result);
+
+    const activityHistory = document.getElementById('activity-data');
+    const div = document.createElement('div');
+    const currentTime = new Date().toLocaleTimeString();
+    const addTitle = document.getElementById('dark-title');
+    const darkTitle = addTitle.innerText
+    div.classList.add('mt-10');
+    div.innerHTML = `
+    
+       <p class="bg-blue-100 rounded-lg shadow-lg p-3">You have Complete The Task ${darkTitle} at ${currentTime}</p>
+      `
+    activityHistory.appendChild(div);
+    const btn1 = document.getElementById('completed-btn2');
     btn1.disabled=true;
     btn1.style.backgroundColor = "gray";
 })
